3227c3bb31a6cb5862590f22b1080e2f9964f05b
Liam <sheeralm@mail.uc.edu>
2023-11-09 20:09:55 -0500

    designed homepage and toolbar

---
+++ js/home-background.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("home-wrapper");
+
+function circles() {
+    let e = document.createElement('div');
+    e.setAttribute('id', 'circle');
+    canvas.appendChild(e);
+}
+
+circles();
