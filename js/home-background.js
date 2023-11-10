73ba5bea529c8d87eeb570d014e12b16e99b9515
Liam <sheeralm@mail.uc.edu>
2023-11-10 03:40:47 -0500

    updated home page with graphic of desk

--- js/home-background.js
+++ js/home-background.js
@@ -1,9 +1,19 @@
-let canvas = document.getElementById("home-wrapper");
+let svg = document.getElementById("monitor-1");
+let screenSaver = document.getElementById("screen-saver");
 
-function circles() {
-    let e = document.createElement('div');
-    e.setAttribute('id', 'circle');
-    canvas.appendChild(e);
+function updateDVDPosition() {
+    let svgSpecs = svg.getBoundingClientRect();
+    screenSaver.style.width = svgSpecs.width;
+    screenSaver.style.height = svgSpecs.height;
+    screenSaver.style.top = svgSpecs.top + document.body.scrollTop;
+    screenSaver.style.right = svgSpecs.right;
+    screenSaver.style.bottom = svgSpecs.bottom;
+    screenSaver.style.left = svgSpecs.left;
+    // setTimeout(function () {
+    // }, 5000);
 }
+updateDVDPosition();
 
-circles();
+window.addEventListener("resize", (event) => {
+    updateDVDPosition();
+});
