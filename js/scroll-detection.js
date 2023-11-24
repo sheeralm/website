a4c23a09d96e01b76fb09abd9733b7067a985918
Liam <sheeralm@mail.uc.edu>
2023-11-24 14:54:23 -0500

    toolbar buttons lower based on active section

--- js/scroll-detection.js
+++ js/scroll-detection.js
@@ -1,19 +1,18 @@
-let test = document.querySelector(".scroller").querySelectorAll("section");
-//
+let oldWrapper = "";
 
-// for (let i = 0; i < test.length; i++) {
-//     test[i].addEventListener('scrollend', () => console.log('snap ended'));
-// }
-let selections = ["Home","About","Projects","Resume","Contact"];
-let head = selections[0];
-let scroller = document.querySelector(".scroller");
-let headY = scroller.scrollTop;
-scroller.addEventListener('scrollend', () => {
-    if (scroller.scrollTop > headY) {
-        console.log("scroll down");
-    }
-    else {
-        console.log("scroll up");
-    }
-    headY = scroller.scrollTop;
-});
+$('article').scroll(function () {
+    $('section').each(function () {
+        if($(this).position().top <= $(document).scrollTop() && ($(this).position().top + $(this).outerHeight()) > $(document).scrollTop()) {
+            let newWrapper = $(this).attr("id");
+            if (newWrapper != oldWrapper) {
+                currentButton = $('#' + newWrapper.substring(0, newWrapper.indexOf('-')));
+                currentButton.toggleClass("active");
+                if (oldWrapper != "") {
+                    oldButton = $('#' + oldWrapper.substring(0, oldWrapper.indexOf('-')));
+                    oldButton.toggleClass("active");
+                }
+                oldWrapper = newWrapper;
+            }
+        }
+    });
+})
