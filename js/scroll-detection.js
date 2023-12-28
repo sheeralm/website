3d90c896fe8681aae57c1abb837442f3fe178a46
Liam <sheeralm@mail.uc.edu>
2023-12-28 14:42:50 -0500

    updated about me section

--- js/scroll-detection.js
+++ js/scroll-detection.js
@@ -1,8 +1,34 @@
 let oldWrapper = "";
-
+let oldScrollTopVal = 0;
+let isScrollUp = false;
 $('article').scroll(function () {
     $('section').each(function () {
-        if($(this).position().top <= $(document).scrollTop() && ($(this).position().top + $(this).outerHeight()) > $(document).scrollTop()) {
+        let scrollTopVal = $(".scroller").scrollTop() - 15;
+        let screenBottom = scrollTopVal + $(window).height();
+        let sectionBottom = this.offsetTop + $(this).outerHeight();
+        // console.log(this.id);
+        console.log("scroll top " + scrollTopVal);
+        // console.log("section bottom " + sectionBottom);
+        // console.log("screen bottom " + screenBottom);
+        // if (scrollTopVal <= oldScrollTopVal) {
+        //     // if the top of the current section is less than or equal to the scroll top val (the current height of the top of the screen)
+        //     // and if the top of the current section + the height of the section is less than the position of the top of the screen
+        //     if ($(this).position().top <= scrollTopVal && ($(this).position().top + $(this).outerHeight()) > scrollTopVal) {
+        //         let newWrapper = $(this).attr("id");
+        //         if (newWrapper != oldWrapper) {
+        //             currentButton = $('#' + newWrapper.substring(0, newWrapper.indexOf('-')));
+        //             currentButton.toggleClass("active");
+        //             if (oldWrapper != "") {
+        //                 oldButton = $('#' + oldWrapper.substring(0, oldWrapper.indexOf('-')));
+        //                 oldButton.toggleClass("active");
+        //             }
+        //             oldWrapper = newWrapper;
+        //         }
+        //     }
+        // }
+        // else {
+        if (sectionBottom > screenBottom && (sectionBottom - $(this).outerHeight()) < screenBottom) {
+            console.log(this.id);
             let newWrapper = $(this).attr("id");
             if (newWrapper != oldWrapper) {
                 currentButton = $('#' + newWrapper.substring(0, newWrapper.indexOf('-')));
@@ -14,5 +40,7 @@ $('article').scroll(function () {
                 oldWrapper = newWrapper;
             }
         }
+        // }
+        oldScrollTopVal = scrollTopVal;
     });
 })
